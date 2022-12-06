48d58be83900a02f377cffe3be15543e1236f451
Julia-Marianne <115991440+Julia-Marianne@users.noreply.github.com>
2022-12-06 18:48:55 +0000

    update

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -9,16 +9,16 @@
 
 
 //localhost database credentials
-// define('HOST', 'localhost');
-// define('PASS', '');
-// define('DB', 'shoppn');
-// define('USER', 'root');
+define('HOST', 'localhost');
+define('PASS', '');
+define('DB', 'shoppn');
+define('USER', 'root');
 
 //heroku credentials
 
-define('HOST', 'us-cdbr-east-06.cleardb.net');
-define('PASS', '52db3200');
-define('DB', 'heroku_9df0c08f94da864');
-define('USER', 'b0827807698ac6');
+// define('HOST', 'us-cdbr-east-06.cleardb.net');
+// define('PASS', '52db3200');
+// define('DB', 'heroku_9df0c08f94da864');
+// define('USER', 'b0827807698ac6');
 
 ?>
